eb83da7a7a3944353fa1d365c5e149fe3bbf66ab
gitmsx <47451157+gitmsx@users.noreply.github.com>
2020-07-01 17:03:36 +0300

    000001 commit

---
+++ 0001.bat
@@ -0,0 +1,7 @@
+﻿echo "# testuni001" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/gitmsx/testuni001.git
+git push -u origin master
+                
